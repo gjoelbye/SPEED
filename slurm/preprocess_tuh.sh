c94e8de43c6b3bc944da7a4fad4afdbce49be11d
linsk <linsk@dtu.dk>
2025-10-14 21:01:34 +0200

    config and slurm updates

--- slurm/preprocess_tuh.sh
+++ slurm/preprocess_tuh.sh
@@ -3,8 +3,8 @@
 #SBATCH --partition=cyclopes
 #SBATCH --job-name=tuh_30s
 #SBATCH --output=/scratch/linsk/tuh_test_preprocessed/logs/slurm-%J.out
-#SBATCH --cpus-per-task=4 
-#SBATCH --mem=32gb
+#SBATCH --cpus-per-task=8 
+#SBATCH --mem=64gb
 #SBATCH --mail-user=linsk@dtu.dk
 #SBATCH --export=ALL
 #SBATCH --time=144:00:00
@@ -15,6 +15,6 @@ echo "Start: $(date +%F-%R:%S)"
 echo -e "Working dir: $(pwd)\n"
 
 source ~/.bashrc
-python scripts/preprocess_hbn.py --config configs/tuh_test_30s.yaml
+python scripts/preprocess_hbn.py --config configs/tuh_30s.yaml
 
 echo "Done: $(date +%F-%R:%S)"
