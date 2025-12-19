6bb6024b1a7d12ef7682fe27064e53f1c22fc5b1
Gjoelbye <andersgm@protonmail.com>
2025-12-19 16:01:56 +0100

    Refactor for Downstream

--- slurm/preprocess_tuh.sh
+++ slurm/preprocess_tuh.sh
@@ -5,7 +5,7 @@
 #SBATCH --job-name=tuh_60s
 #SBATCH --output=/scratch/agjma/TUH_SPEED/logs/slurm-%J.out
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=48
+#SBATCH --cpus-per-task=24
 #SBATCH --exclusive
 #SBATCH --mem=0
 #SBATCH --mail-user=agjma@dtu.dk
